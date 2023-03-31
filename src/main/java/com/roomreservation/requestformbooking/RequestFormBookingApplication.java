713916a3f1cfb5cc841304e6ee8f22f597f0a7fc
viduth <it21260056@my.sliit.lk>
2023-03-31 21:28:42 +0530

    reservation form

--- src/main/java/com/roomreservation/requestformbooking/RequestFormBookingApplication.java
+++ src/main/java/com/roomreservation/requestformbooking/RequestFormBookingApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RequestFormBookingApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RequestFormBookingApplication.class, args);
+		System.out.println("Viduth");
 	}
 
 }
