7249e2ba8ee1c9d9c8e13faa31a8d779bd20c175
viduthranaweera <it21260056@my.sliit.lk>
2023-04-19 03:45:00 +0530

    sat 08

--- src/main/java/com/roomreservation/requestformbooking/controller/UserController.java
+++ src/main/java/com/roomreservation/requestformbooking/controller/UserController.java
@@ -31,6 +31,33 @@ public class UserController {
                 .orElseThrow(()->new UserNotFoundException(id));
     }
 
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser,@PathVariable Long id){
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setName(newUser.getName());
+                    user.setCheckInDate(newUser.getCheckInDate());
+                    user.setCheckOutDate(newUser.getCheckOutDate());
+                    user.setCheckOutDate(newUser.getCheckOutDate());
+                    user.setType(newUser.getType());
+                    user.setNoOfPerson(newUser.getNoOfPerson());
+                    user.setNoOfRooms(newUser.getNoOfRooms());
+
+                    return userRepository.save(user);
+
+                }).orElseThrow(()->new UserNotFoundException(id));
+
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "User with id "+id+"has been deleted success";
+    }
+
 }
 
 
