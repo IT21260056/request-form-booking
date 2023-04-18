c34aa856e77ed26b607a43a7f3dacee16a3e5eef
viduthranaweera <it21260056@my.sliit.lk>
2023-04-18 17:33:52 +0530

    sat 08

--- src/main/java/com/roomreservation/requestformbooking/controller/UserController.java
+++ src/main/java/com/roomreservation/requestformbooking/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.roomreservation.requestformbooking.controller;
 
+import com.roomreservation.requestformbooking.exception.UserNotFoundException;
 import com.roomreservation.requestformbooking.model.User;
 import com.roomreservation.requestformbooking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,8 +21,18 @@ public class UserController {
 
     @GetMapping("/users")
     List<User> getAllUsers(){
+
         return userRepository.findAll();
     }
+
+    @GetMapping("/user/{id}")
+    User getuserById(@PathVariable Long id){
+        return userRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
 }
+
+
 
 
