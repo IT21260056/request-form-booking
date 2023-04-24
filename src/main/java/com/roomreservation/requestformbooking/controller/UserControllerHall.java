9920cce6ec9d245e0247d073886344b6e347b022
viduthranaweera <it21260056@my.sliit.lk>
2023-04-24 21:17:55 +0530

    sat 08

--- src/main/java/com/roomreservation/requestformbooking/controller/UserControllerHall.java
+++ src/main/java/com/roomreservation/requestformbooking/controller/UserControllerHall.java
@@ -1,14 +1,13 @@
 package com.roomreservation.requestformbooking.controller;
 
+import com.roomreservation.requestformbooking.exception.UserNotFoundException;
 import com.roomreservation.requestformbooking.model.HallBooking;
 //import com.roomreservation.requestformbooking.model.User;
-import com.roomreservation.requestformbooking.repository.UserRepository;
 import com.roomreservation.requestformbooking.repository.UserRepositoryHall;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -22,5 +21,19 @@ public class UserControllerHall {
     HallBooking newHallBooking(@RequestBody HallBooking newHallBooking){
         return userRepositoryHall.save(newHallBooking);
     }
+
+    @GetMapping("/users1")
+    List<HallBooking> getAllUsers(){
+        return userRepositoryHall.findAll();
+
+//        return userRepositoryHall.findAll();
+    }
+
+    @GetMapping("/user1/{id}")
+    HallBooking getuserById(@PathVariable Long id){
+        return userRepositoryHall.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
 }
+
 
