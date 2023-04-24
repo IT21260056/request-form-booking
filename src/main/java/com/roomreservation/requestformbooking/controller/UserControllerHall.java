feec9f28b9c890203488cb8e3011dd44fc36b39e
viduthranaweera <it21260056@my.sliit.lk>
2023-04-24 23:16:54 +0530

    sat 08

--- src/main/java/com/roomreservation/requestformbooking/controller/UserControllerHall.java
+++ src/main/java/com/roomreservation/requestformbooking/controller/UserControllerHall.java
@@ -3,6 +3,7 @@ package com.roomreservation.requestformbooking.controller;
 import com.roomreservation.requestformbooking.exception.UserNotFoundException;
 import com.roomreservation.requestformbooking.model.HallBooking;
 //import com.roomreservation.requestformbooking.model.User;
+import com.roomreservation.requestformbooking.model.User;
 import com.roomreservation.requestformbooking.repository.UserRepositoryHall;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -25,14 +26,39 @@ public class UserControllerHall {
     @GetMapping("/users1")
     List<HallBooking> getAllUsers(){
         return userRepositoryHall.findAll();
-
-//        return userRepositoryHall.findAll();
     }
 
     @GetMapping("/user1/{id}")
     HallBooking getuserById(@PathVariable Long id){
         return userRepositoryHall.findById(id)
                 .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+//    @PutMapping("/user/{id}")
+//    User updateUser(@RequestBody User newUser, @PathVariable Long id){
+//        return userRepository.findById(id)
+//                .map(user -> {
+//                    user.setName(newUser.getName());
+//                    user.setCheckInDate(newUser.getCheckInDate());
+//                    user.setCheckOutDate(newUser.getCheckOutDate());
+//                    user.setCheckOutDate(newUser.getCheckOutDate());
+//                    user.setType(newUser.getType());
+//                    user.setNoOfPerson(newUser.getNoOfPerson());
+//                    user.setNoOfRooms(newUser.getNoOfRooms());
+//
+//                    return userRepository.save(user);
+//
+//                }).orElseThrow(()->new UserNotFoundException(id));
+//
+//    }
+
+    @DeleteMapping("/user1/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepositoryHall.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepositoryHall.deleteById(id);
+        return "User with id "+id+"has been deleted success";
     }
 }
 
