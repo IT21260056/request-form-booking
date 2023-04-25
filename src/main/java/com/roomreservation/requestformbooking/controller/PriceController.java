6c0d84bece9882423b53e71a32b6877fad29db2a
viduthranaweera <it21260056@my.sliit.lk>
2023-04-26 01:54:39 +0530

    sat 08

--- src/main/java/com/roomreservation/requestformbooking/controller/PriceController.java
+++ src/main/java/com/roomreservation/requestformbooking/controller/PriceController.java
@@ -1,12 +1,11 @@
 package com.roomreservation.requestformbooking.controller;
 
+import com.roomreservation.requestformbooking.exception.UserNotFoundException;
 import com.roomreservation.requestformbooking.model.RoomPrice;
+import com.roomreservation.requestformbooking.model.User;
 import com.roomreservation.requestformbooking.repository.PriceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -17,6 +16,25 @@ public class PriceController {
     @PostMapping("/price")
     RoomPrice newRoomPrice(@RequestBody RoomPrice newRoomPrice){
         return priceRepository.save(newRoomPrice);
+    }
+
+    @GetMapping("/price/{id}")
+    RoomPrice getuserById(@PathVariable Long id){
+        return priceRepository.findById(id)
+                .orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/price/{id}")
+    RoomPrice updateUser(@RequestBody RoomPrice newRoomPrice,@PathVariable Long id){
+        return priceRepository.findById(id)
+                .map(price -> {
+                    price.setRoomType(newRoomPrice.getRoomType());
+                    price.setPrice(newRoomPrice.getPrice());
+
+                    return priceRepository.save(price);
+
+                }).orElseThrow(()->new UserNotFoundException(id));
+
     }
 
 }
